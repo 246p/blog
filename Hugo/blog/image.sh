045db4e89154eac3cf2f23785f049d1bc5bf507f
246p <0016kmj@gmail.com>
2024-02-02 01:59:27 +0900

    rebuilding site 2024년 2월  2일 금요일 01시 59분 27초 KST

--- Hugo/blog/image.sh
+++ Hugo/blog/image.sh
@@ -2,7 +2,9 @@
 if [ $# -eq 1 ]
 then
     mv $1 ./public/image
-    echo "./../../image/$1"
+    echo -n "./../../image/$1" | pbcopy
+
+    echo "path copied"
 else
     echo "usage : ./image.sh image_file"
 fi
